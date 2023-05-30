c53cfe38a6d5f63025dd039eb549644773682721
Laurence Cortez <laurencecortez.com>
2023-05-30 23:08:58 +0800

    handle post request pokemon region

--- netlify/functions/get-pokedex.js
+++ netlify/functions/get-pokedex.js
@@ -1,10 +1,11 @@
 const { default: axios } = require('axios');
 
-exports.handler = async () => {
-  const POKE_API = 'https://pokeapi.co/api/v2/pokedex/kanto';
+exports.handler = async (event, context) => {
+  const eventBody = JSON.parse(event.body);
+  const POKE_API = 'https://pokeapi.co/api/v2/pokedex/' + eventBody.region;
 
   const response = await axios(POKE_API);
-  const data = await response.data;
+  const data = await response.data.pokemon_entries;
   console.log(data);
   return {
     statusCode: 200,
